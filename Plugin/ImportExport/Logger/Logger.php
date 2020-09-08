8ef55ab82f62f463f5b8af69d1c00e4698076123
Dmitry Fedyuk <dfediuk@gmail.com>
2020-09-08 20:00:00 +0600

    0.0.3: https://github.com/dxmoto/site/issues/49

---
+++ Plugin/ImportExport/Logger/Logger.php
@@ -0,0 +1,23 @@
+<?php
+namespace DxMoto\Core\Plugin\ImportExport\Logger;
+use Firebear\ImportExport\Logger\Logger as Sb;
+# 2020-09-08 "Prevent `Firebear_ImportExport` from logging non-error messages": https://github.com/dxmoto/site/issues/49
+final class Logger {
+	/**
+	 * 2020-09-08
+	 * @see \Firebear\ImportExport\Logger\Logger::info()
+	 */
+	function aroundDebug() {}
+
+	/**
+	 * 2020-09-08
+	 * @see \Firebear\ImportExport\Logger\Logger::info()
+	 */
+	function aroundInfo() {}
+
+	/**
+	 * 2020-09-08
+	 * @see \Firebear\ImportExport\Logger\Logger::info()
+	 */
+	function aroundSuccess() {}
+}

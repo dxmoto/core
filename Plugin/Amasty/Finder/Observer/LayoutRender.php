53793ad849adcfc836257b39006410a7354eb6c6
Dmitry Fedyuk <dfediuk@gmail.com>
2020-09-14 15:32:39 +0600

    0.0.8: https://github.com/dxmoto/core/issues/7

--- Plugin/Amasty/Finder/Observer/LayoutRender.php
+++ Plugin/Amasty/Finder/Observer/LayoutRender.php
@@ -4,10 +4,13 @@ use Amasty\Finder\Observer\LayoutRender as Sb;
 use Magento\Framework\Event\Observer as O;
 /**
  * 2020-09-12
- * 1) "`Ess_M2ePro`: «Broken reference: the 'amasty.finder.5' element cannot be added as child to 'content',
+ * 1) "`Amasty_Finder`: «Broken reference: the 'amasty.finder.5' element cannot be added as child to 'content',
  * because the latter doesn't exist»": https://github.com/dxmoto/core/issues/7
  * 2) The problem action is `m2epro_cron_index`:
  * curl --insecure "https://localhost.com:2186/M2ePro/cron?auth_key=<…>&connection_id=$RANDOM"
+ * 2020-09-14
+ * The issue is occured not only with Ess_M2ePro HTTP requests, but with other HTTP requests too, e.g. /robots.txt:
+ * https://github.com/dxmoto/core/issues/7#issuecomment-691905513
  */
 final class LayoutRender {
 	/**
@@ -18,5 +21,5 @@ final class LayoutRender {
 	 * @param O $o
 	 * @return O
 	 */
-	function aroundExecute(Sb $sb, \Closure $f, O $o) {return df_action_prefix('m2epro') ? $o : $f($o);}
+	function aroundExecute(Sb $sb, \Closure $f, O $o) {return df_action_prefix('catalog') ? $f($o) : $o;}
 }
